6fbe614c6d04e83a74e931607bf46fcbe9366a8d
GUOQING CAO <gc85@alu.ua.es>
2024-02-15 10:23:59 +0100

    sesion 2

---
+++ hada-02/main.cs
@@ -0,0 +1,12 @@
+
+using System;
+namespace hadaTeoSes2{
+	class Test{
+		static void Main(){
+			Console.WriteLine("Hola");
+			Console.Write(", que tal?");
+			Console.ReadLine();
+			// mi comentario
+		}
+	}
+}
